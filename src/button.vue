e017df0f91b86bd40e365ac6e8a944c73279e968
Seth <1532084282@qq.com>
2019-06-04 23:17:12 +0800

    add circle button

--- src/button.vue
+++ src/button.vue
@@ -1,32 +1,33 @@
 <template>
   <div>
     <button class="el-button" :class="`${type}`">
-    <svg class="icon">
-     <use xlink:href="#icon-error"></use>
-    </svg>
-      <slot>默认</slot>
+      <svg class="icon">
+        <use xlink:href="#icon-error"></use>
+      </svg>
+      <slot></slot>
     </button>
   </div>
 </template>
 
 <script>
-import './svg.js'
+import "./svg.js";
 export default {
   name: "Button",
-  props:['type'],
+  props: ["type"]
 };
 </script>
 
 <style>
+/* base css */
 .el-button {
   margin: 1em;
   display: inline-block;
   line-height: 1;
   text-align: center;
   outline: none;
-  border: 1px solid var(--border-color);
+  background-color: var(--button-normal);
+  border: 1px solid #dcdfe6;
   color: var(--color);
-  background-color: var(--button-danger-background);
   padding: var(--padding-topandbottom) var(--padding-leftandright);
   font-size: var(--font-size);
   border-radius: var(--border-radius);
@@ -43,17 +44,47 @@ button:active::before {
   height: 100%;
   background: rgba(0, 0, 0, 5%);
 }
+
+button:hover::after {
+  display: block;
+  content: "";
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 15%);
+}
+
+button.circle:hover::after, button.circle:active::before {
+  border-radius:50%;
+}
+
 .icon {
   width: 1em;
   height: 1em;
   vertical-align: -0.15em;
   fill: currentColor;
   overflow: hidden;
-  }
-.normal{
- color: black;
- background-color: white;
- border: 1px solid #dcdfe6;
+}
+
+/* diffrent type button */
+button.danger {
+  background-color: var(--button-danger);
+}
+button.normal {
+  color: black;
+  background-color: var(--button-normal);
+  border: 1px solid #dcdfe6;
+}
+button.success {
+  background-color: var(--button-success);
+  border: 1px solid var(--button-success);
+}
+/* circle button */
+button.circle {
+  border-radius: 50%;
+  padding: var(--circle-padding);
 }
 </style>
 
