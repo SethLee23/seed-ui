25ac2b9ae26de92f999926aed2f2e28d58bd105b
Seth <1532084282@qq.com>
2019-06-16 11:04:13 +0800

    将代码放到travisci自动检测，修改button.test.js

--- test/button.test.js
+++ test/button.test.js
@@ -1,4 +1,4 @@
-const expect = chai.expect;
+let expect = chai.expect;
 import Vue from 'vue'
 import Button from '../src/button/button'
 
@@ -14,32 +14,25 @@ describe('Button', () => {
     it('可以设置icon.', () => {
         //创建构造函数，生成实例，传递需要测试的参数，mount到文档中
         //获取元素，获取元素的属性，使用断言，destroy实例
-        const Constructor = Vue.extend(Button)
+        let Constructor = Vue.extend(Button)
         let vm = new Constructor({
-            propsData: {
-                icon: 'setting',
-            }
-        }).$mount()
-        let useElement = vm.$el.querySelector('use')
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
         propsData: {
             icon: 'setting'
         }
         }).$mount()
-        const useElement = vm.$el.querySelector('use')
+        let useElement = vm.$el.querySelector('use')
         expect(useElement.getAttribute('xlink:href')).to.equal('#icon-setting')
         vm.$destroy()
     })
     it('可以设置loading.', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
         propsData: {
             icon: 'setting',
             loading: true
         }
         }).$mount()
-        const useElements = vm.$el.querySelectorAll('use')
+        let useElements = vm.$el.querySelectorAll('use')
         expect(useElements.length).to.equal(1)
         expect(useElements[0].getAttribute('xlink:href')).to.equal('#icon-loading')
         vm.$destroy()
@@ -47,38 +40,38 @@ describe('Button', () => {
     it('icon 默认的 order 是 1', () => {
         // 需要测试css就需要mount到div在mount到文档中
         // 获取css属性，getComputedStyle
-        const div = document.createElement('div')
+        let div = document.createElement('div')
         document.body.appendChild(div)
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
         propsData: {
             icon: 'settings',
         }
         }).$mount(div)
-        const icon = vm.$el.querySelector('svg')
+        let icon = vm.$el.querySelector('svg')
         expect(getComputedStyle(icon).order).to.eq('1')
         vm.$el.remove()
         vm.$destroy()
     })
     it('设置 position 可以改变 order', () => {
-        const div = document.createElement('div')
+        let div = document.createElement('div')
         document.body.appendChild(div)
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
         propsData: {
             icon: 'setting',
             position: 'right'
         }
         }).$mount(div)
-        const icon = vm.$el.querySelector('svg')
+        let icon = vm.$el.querySelector('svg')
         expect(getComputedStyle(icon).order).to.eq('2')
         vm.$el.remove()
         vm.$destroy()
     })
     it('点击 button 触发 click 事件', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor().$mount()
-        const callback = sinon.fake();
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor().$mount()
+        let callback = sinon.fake();
         vm.$on('click', callback)
         vm.$el.click()
         // vm.$el此时相当于组件
