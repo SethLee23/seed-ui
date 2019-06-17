323a0a9f1bf12cab0734162ebee0b5ee496e7607
Seth <1532084282@qq.com>
2019-06-17 15:01:59 +0800

    基本完成layout,加入button，transition

--- src/layout/layout-sider.vue
+++ src/layout/layout-sider.vue
@@ -1,26 +1,42 @@
 <template>
-<div class="SLayoutSider">
-      <slot></slot> 
-</div>
+  <transition name="slide">
+    <div class="SLayoutSider" v-if="show">
+      <slot></slot>
+      <button @click="triggerShow">trigger</button>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
-  name: 'SLayoutSider',
-  data(){
+  name: "SLayoutSider",
+  data() {
     return {
+      show: true
+    };
+  },
+  props: {},
+  mounted() {},
+  methods: {
+    triggerShow() {
+      this.show = !this.show;
     }
-  },
-  props: {
-  },
-  mounted() {
-  },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .SLayoutSider {
 }
+// 过渡生效状态
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.5s;
+}
+//进入过渡和过渡结束状态
+.slide-enter, .slide-leave-to {
+  margin-right: -200px;
+}
+
 </style>
 
 
