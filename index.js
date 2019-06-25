85ce3a42bc25c5acb5cde57361ba1d5320c20a4b
Seth <1532084282@qq.com>
2019-06-25 14:24:44 +0800

    修正index.js

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+
+import Icon from './src/button/icon'
 import Button from './src/button/button'
 import ButtonGroup from './src/button/button-group'
-import Icon from './src/button/icon'
-import Input from './src/Input/input'
+import Input from './src/input/input'
+import Tabs from './src/tabs/tabs'
+import TabsHead from './src/tabs/tabs.head'
+import TabsItem from './src/tabs/tabs.item'
+import TabsPane from './src/tabs/tabs.pane'
+import TabsBody from './src/tabs/tabs.body'
+import Layout from './src/layout/layout'
+import LayoutHeader from './src/layout/layout-header'
+import LayoutSider from './src/layout/layout-sider'
+import LayoutContent from './src/layout/layout-content'
+import LayoutFooter from './src/layout/layout-footer'
+import Upload from './src/picture-bed/upload'
+import Row from './src/grid/row'
+import Col from './src/grid/col'
+import plugin from './src/toast/plugin'
+import Popover from './src/popover/popover'
+import Collapse from './src/collapse/collapse'
+import CollapseItem from './src/collapse/collapse-item'
 
-export {Button,ButtonGroup,Icon,Input}
+Vue.use(plugin)
+
+export {
+    Button,
+    ButtonGroup,
+    Icon,
+    Input,
+    Tabs,
+    TabsHead,
+    TabsItem,
+    TabsPane,
+    TabsBody,
+    Layout,
+    LayoutHeader,
+    LayoutSider,
+    LayoutContent,
+    LayoutFooter,
+    Upload,
+    Row,
+    Col,
+    plugin,
+    Popover,
+    Collapse,
+    CollapseItem
+}
